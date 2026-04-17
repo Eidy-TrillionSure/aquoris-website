d37dd81c75193d026d66ac92610a031011f740c9
Benjapol Worakan <b.e.n.wrk@gmail.com>
2026-04-17 12:02:13 +0000

    Fix footer logo opacity

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     <footer className="border-t bg-background">
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <a href="#" className="flex items-center space-x-2">
-          <Logo className="h-8 w-auto opacity-50" />
+          <Logo className="h-8 w-auto" />
         </a>
         <p className="text-center text-sm leading-loose text-muted-foreground md:text-right">
           &copy; {new Date().getFullYear()} Aquoris Intelligence. All rights reserved.
